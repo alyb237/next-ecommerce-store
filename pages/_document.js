6cc3cfaa80c78cccfad7e0a26d4baa63f6fa1e16
alyb237 <alyb237@gmail.com>
2022-06-18 16:33:01 +0200

    Fix preflight issues

--- pages/_document.js
+++ pages/_document.js
@@ -19,7 +19,7 @@ export default function MyDocument() {
         />
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Quicksand:wght@500&display=swap"
           rel="stylesheet"
